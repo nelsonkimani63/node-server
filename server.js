4f58686fb0197f2ac38de449190bd09f81930096
nelsonkimani63 <nelsonkimani63@gmail.com>
2022-06-24 11:32:55 +0300

    placed the static files in public folder to create an express server

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+//Using Express to Create Server 
 const express = require("express"); //import express
 const server = express();
+const PORT = 5000;
+
+
+server.use(express.static('public'));
 
 server.get("/", function(req, res) {
-    res.send("Welcome to Express Server");
+    res.send("Welcome to Nelson's Express Server");
 
 });
 
 
 
-server.listen(5000, () => {
-    console.log("Server is listening on port 5000")
+
+server.listen(PORT, function() {
+    return console.log(`Server is listening on port: ${PORT}`);
 });
